66474e68104cec2a4f763ad2303c6f949fafac07
Yongseung Song <yongseung.song@icloud.com>
2023-11-23 14:55:33 +0900

    feat: 입력창 추가

--- src/components/inputBox/InputBox.style.jsx
+++ src/components/inputBox/InputBox.style.jsx
@@ -2,10 +2,13 @@ import styled from "styled-components";
 
 export const InputBox = styled.div`
   width: 100%;
-  height: 60px; // 조건부 스타일링 필요
+  height: 160px; // 조건부 스타일링 필요
   position: sticky;
   top: 84px;
   margin-bottom: 16px;
   background-color: #fff;
   border: 1px solid #000;
+  textarea {
+    width: 100%;
+  }
 `;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import Main from "../components/main/Main";
 import Sidebar from "../components/sidebar/Sidebar";
-import app from "../firebase";
+import { app } from "../firebase";
 
 const StHomeContainer = styled.div`
   position: relative;
