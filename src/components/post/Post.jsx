c69f527b0d0feceb8525f29f950b79e7d655acb2
KANG0417 <ipia99@naver.com>
2023-11-23 17:02:18 +0900

    수정

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import * as St from "./Post.style";
+import Button from "../../shared/button/Button";
 
 function Post() {
-  return <St.PostWrapper>Post</St.PostWrapper>;
+  const [] = useState("");
+  const [] = useState("");
+
+  const addClick = () => {
+    alert("추가되었습니다!");
+  };
+
+  const delClick = () => {
+    alert("삭제되었습니다!");
+  };
+
+  return (
+    <St.PostWrapper>
+      {/* <form>
+        제목: <input type="text" onChange={}/>
+        <br />
+        내용: <input type="text" onChange={}/>
+        <br />
+      </form> */}
+      {/* <Button clickBtnHandler={addClick}>등록</Button> */}
+    </St.PostWrapper>
+  );
 }
 
 export default Post;
