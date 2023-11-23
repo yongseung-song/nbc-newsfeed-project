c7cfda00465869f2b3f673aea0fa0465528b61c7
BLACKPA1NT <yeo233@gmail.com>
2023-11-23 10:55:58 +0900

    modal생성

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import * as St from "./Header.style.jsx";
+import ModalBasic from "./ModalBasic.jsx";
 
 function Header() {
-  return (
-    <St.Header>
-      <Link to={"/"}>home</Link>
-      <form action="submit">
-        <label htmlFor="search">검색</label>
-        <input id="search" name="search" type="text" />
-      </form>
-      <St.BtnContainer>
-        <button>darkmode</button>
-        <button>login</button>
-      </St.BtnContainer>
-    </St.Header>
-  );
+	const [modalOpen, setModalOpen] = useState(false);
+
+	const loginModalHandler = () => {
+		setModalOpen(true);
+	};
+
+	return (
+		<St.Header>
+			<Link to={"/"}>home</Link>
+			<form action="submit">
+				<label htmlFor="search">검색</label>
+				<input id="search" name="search" type="text" />
+			</form>
+			<St.BtnContainer>
+				<button>darkmode</button>
+				<button onClick={loginModalHandler}>login</button>
+				{modalOpen && <ModalBasic setModalOpen={setModalOpen} />}
+			</St.BtnContainer>
+		</St.Header>
+	);
 }
 
 export default Header;
