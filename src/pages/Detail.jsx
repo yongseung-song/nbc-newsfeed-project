a488ab969f5597374eb9989eedd65f8370fd7a12
lsw502 <tkddnr1216@naver.com>
2023-11-24 16:51:05 +0900

    수정 삭제 버튼

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import styled from "styled-components";
 
 function Detail() {
-  const deleteBtnHandler = () => {
-    const answer = window.confirm("정말로 삭제하시겠습니까?");
-    if (!answer) return;
-  };
   const EditDone = () => {
     if (!editingText) return alert("수정사항이 없습니다.");
   };
+
+  const deleteBtnHandler = () => {
+    const newLetters = letters.filter((letter) => letter.id !== id);
+    setLetters(newLetters);
+    const answer = window.confirm("정말로 삭제하시겠습니까?");
+    if (!answer) return;
+  };
+
   return (
     <>
       <BtnsWrapper>
