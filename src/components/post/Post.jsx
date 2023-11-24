c797b295488d6685fe247bafd9bd5075fa511b6b
Yongseung Song <yongseung.song@icloud.com>
2023-11-25 00:00:03 +0900

    feat: post 모달창 구현

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,14 +1,13 @@
 import React, { useContext } from "react";
-import { createPortal } from "react-dom";
 import { ModalContext } from "../../context/ModalContext";
-import * as St from "./Post.style";
-import Button from "../../shared/button/Button";
 import Tag from "../tag/Tag";
+import * as St from "./Post.style";
+import PostModal from "./PostModal";
 
 function Post({ id, creator, title, content, date, tag }) {
-  const { showModal, setShowModal } = useContext(ModalContext);
+  const { showPostModal, setShowPostModal } = useContext(ModalContext);
   const postClickHandler = () => {
-    setShowModal(true); // 이부분때문에 포스트 누르면 모달 뜸
+    setShowPostModal(true); // 이부분때문에 포스트 누르면 모달 뜸
   };
 
   return (
@@ -34,6 +33,16 @@ function Post({ id, creator, title, content, date, tag }) {
             })}
         </St.TagContainer>
       </St.PostWrapper>
+      {showPostModal && (
+        <PostModal
+          id={id}
+          creator={creator}
+          title={title}
+          content={content}
+          date={date}
+          tag={[]}
+        />
+      )}
     </>
   );
 }
--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,10 +1,10 @@
+import dayjs from "dayjs";
+import { collection, getDocs } from "firebase/firestore";
 import React, { useContext, useEffect } from "react";
+import { PostContext } from "../../context/PostContext";
+import { db } from "../../firebase";
 import Post from "../post/Post";
 import * as St from "./Posts.style";
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "../../firebase";
-import dayjs from "dayjs";
-import { PostContext } from "../../context/PostContext";
 
 function Posts() {
   const { postList, setPostList } = useContext(PostContext);
@@ -39,6 +39,14 @@ function Posts() {
             />
           );
         })}
+      <Post
+        id={4312}
+        creator={"나다"}
+        title={"Test"}
+        content={"Dunno"}
+        date={"Whatever"}
+        tag={[]}
+      />
     </St.PostsWrapper>
   );
 }
