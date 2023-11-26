63da469e3224687bbfbea5ab55a3fb8f191346e4
BLACKPA1NT <yeo233@gmail.com>
2023-11-27 03:59:29 +0900

    small design 수정

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,33 +1,9 @@
-import { default as React, default as React, useContext } from "react";
+import React, { useContext } from "react";
 import styled from "styled-components";
 import { PostContext } from "../../context/PostContext";
 import { colors } from "../../styles/GlobalColors";
 
 function Sidebar() {
-	const { filter, setFilter } = useContext(PostContext);
-
-	const clickSidebarBtnHandler = (e) => {
-		setFilter(e.currentTarget.id);
-	};
-
-	return (
-		<SideBarDiv>
-			<ButtonDiv>
-				<ButtonStyle id={"all"} onClick={clickSidebarBtnHandler}>
-					전체
-				</ButtonStyle>
-				<ButtonStyle id={"developTip"} onClick={clickSidebarBtnHandler}>
-					개발 팁 공유
-				</ButtonStyle>
-				<ButtonStyle id={"sidePj"} onClick={clickSidebarBtnHandler}>
-					사이드 프로젝트 구인
-				</ButtonStyle>
-				<ButtonStyle id={"question"} onClick={clickSidebarBtnHandler}>
-					질문
-				</ButtonStyle>
-			</ButtonDiv>
-		</SideBarDiv>
-	);
 	const { filter, setFilter } = useContext(PostContext);
 
 	const clickSidebarBtnHandler = (e) => {
