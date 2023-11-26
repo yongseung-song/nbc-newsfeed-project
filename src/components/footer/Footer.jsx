b11fd5a13ef0cd7b12370b0ef99a2bb7a7f0ef69
BLACKPA1NT <yeo233@gmail.com>
2023-11-27 04:58:29 +0900

    last design

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import styled from "styled-components";
+import { colors } from "../../styles/GlobalColors";
 
 function Footer() {
 	return (
 		<StFooter>
 			<StIndexWrapper>
-				footer
-				<br />
-				footer
+				<p>Copyright 2023. console.lo9 All rights reserved.</p>
 			</StIndexWrapper>
 		</StFooter>
 	);
@@ -18,12 +17,19 @@ export default Footer;
 const StFooter = styled.footer`
 	width: 100%;
 	height: 100px;
-	background-color: green;
+	background-color: ${colors.backgroundColor};
+	margin-top: 60px;
 `;
 
 const StIndexWrapper = styled.div`
 	display: flex;
-	flex-direction: column;
+	flex-direction: row;
 	justify-content: center;
 	align-items: center;
+	width: 50%;
+	margin: auto;
+	color: ${colors.indexFontColor};
+	p {
+		margin-top: 45px;
+	}
 `;
